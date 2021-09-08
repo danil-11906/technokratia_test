5754ec43c01ae1b35a49c94d21e72bc40aa759ca
Danil Barkov <55778811+danil-11906@users.noreply.github.com>
2021-09-08 14:03:02 +0300

    08.09

---
+++ script.sql
@@ -0,0 +1,8 @@
+insert into type (name) values ('канцтовары');
+insert into type (name) values ('авто-товары');
+insert into type (name) values ('все для дома');
+insert into product (cost, name, state_have, type_id) VALUES ('350','ручки','HAVE',1);
+insert into product (cost, name, state_have, type_id) VALUES ('80','тетрадь','HAVE',1);
+insert into product (cost, name, state_have, type_id) VALUES ('500','коллектор','HAVE',2);
+insert into product (cost, name, state_have, type_id) VALUES ('4000','сцепление','DELETE',2);
+insert into product (cost, name, state_have, type_id) VALUES ('300','вантуз','HAVE',3);
